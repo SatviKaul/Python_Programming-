07477239f6e28713a82744c7dc3eeceb22f2281f
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-01-01 23:38:26 +0530

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+#print() fucntion is used to output text and variables.
+# The '#' symbol is used to Create 1 line comments.
+''' ForMultiLine comments:
+    We use 3 single inverted commas Like this.
+
+    This is NOT a way to create comments in Python This is treated as a normal string.
+    But because we are not assigning it to a string it does not affect the code.'''
+
+print("Hello World")
