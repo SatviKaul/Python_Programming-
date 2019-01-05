61c1d9899a611acd036aa7a8485f1dd66d120770
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-01-05 23:18:22 +0530

    Update 4.ListTuplesSets.py

--- 4.ListTuplesSets.py
+++ 4.ListTuplesSets.py
@@ -2,3 +2,7 @@
 #To save a list of variables.
 #Say a list of strings to store names of subjects:
 subjects = ['History' , 'Physics' , 'Maths' , 'Accounts']
+
+#Length of a list can be found by len() function that takes the list's name as the argument
+print(len(subjects))
+print()
