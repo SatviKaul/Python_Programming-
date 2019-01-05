49940afef9ecabf86e0926b84c364eb66710e947
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-01-05 23:20:24 +0530

    Update 4.ListTuplesSets.py

--- 4.ListTuplesSets.py
+++ 4.ListTuplesSets.py
@@ -6,3 +6,6 @@ subjects = ['History' , 'Physics' , 'Maths' , 'Accounts']
 #Length of a list can be found by len() function that takes the list's name as the argument
 print(len(subjects))
 print()
+
+#To access the ith element of list subjects : subjects[i]
+print(subjects[0])
