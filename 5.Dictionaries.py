93f1047079ff058254cb4f60ca1668e3be675043
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-02-08 00:44:59 +0530

    Create 5.Dictionaries.py

---
+++ 5.Dictionaries.py
@@ -0,0 +1,8 @@
+#Dictionaries work upon key value pairs: HASHMAPS or Associative arrays.
+#To make dictionaries : use key-value pairs.
+student = {"name" : "Aman" , "age" : 19 , "subjects" : ["Physics" , "Maths"]}
+#Here all the keys are of string type , 1st value : string , 2nd value : int , 3rd value : list.
+print(student) #Can be simply printed using name
+#To print specific value of a key:
+print(student['name'])
+print()
