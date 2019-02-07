f594601330943723172d3275ab78c1e896bf6a57
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-02-08 00:45:49 +0530

    Update 5.Dictionaries.py

--- 5.Dictionaries.py
+++ 5.Dictionaries.py
@@ -6,3 +6,23 @@ print(student) #Can be simply printed using name
 #To print specific value of a key:
 print(student['name'])
 print()
+#To make a dictionary with integral keys:
+dic = {1 : 7 , 2 : "Hello" , "Ok" : 4 , "Yes" : "No"}
+print(dic[1])
+print()
+
+#Here if we try to access a key that is not present : Error
+#So to avoid error : get() method -> Returns none or a default value.
+print(student.get("phone"))
+print()
+#For a default value: Pass the default value as 2nd argument in the get() method.
+print(student.get("phone" , "Not found"))
+print()
+
+#To ADD/UPDATE a value:
+student["phone"] = 9999999999
+student["name"] = "Rohit"
+print(student)
+print()
+
+#Now to ADD/UPDATE MULTIPLE VALUES at once : update() method.
