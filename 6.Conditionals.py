71e5f36f86d9d6eb3ffedf3bb7e86f87124d11bb
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-02-08 14:09:55 +0530

    Update 6.Conditionals.py

--- 6.Conditionals.py
+++ 6.Conditionals.py
@@ -35,3 +35,51 @@ else:
     print('Else executed')
 #Also note that : else must be just after if statment.
 print()
+
+#Now say we want to print Python for str = 'Python' and Java for str = 'Java' :
+#In python 'else if' is 'elif'.
+str = 'Python'
+if(str == 'Python'):
+    print("Str is Python")
+elif(str == 'Java'):
+    print("Str is Java")
+else:
+    print("Str is none of Python and Java")
+print()
+
+str = 'Java'
+if(str == 'Python'):
+    print("Str is Python")
+elif(str == 'Java'):
+    print("Str is Java")
+else:
+    print("Str is none of Python and Java")
+print()
+
+str = 'C++'
+if(str == 'Python'):
+    print("Str is Python")
+elif(str == 'Java'):
+    print("Str is Java")
+else:
+    print("Str is none of Python and Java")
+print()
+#In this case , if we use if instead of elif, our program will work fine.
+#But if str = 'Python' then by using elif we save time by not checking for the 2nd Condition.
+
+
+#LOGICAL OPERATORS:
+#Logical operators are : 'and' , 'or' and 'not'.
+#In python we use these keyword instead of '&&' , '||' and '!'.
+#Examples :
+a = 5
+b = 3
+if a==5 and b==2:
+    print("Statment 1")
+if a==5 and b==3:
+    print("Statment 2")
+if a==4 or b==3:
+    print("Statment 3")
+if not a==4:
+    print("Statment 4")
+print()
