c65a4e382d8622761d6619933bb8fdeb34039256
pranjalkandhari <31439575+pranjalkandhari@users.noreply.github.com>
2019-03-02 20:42:59 +0530

    Create 8.Functions.py

---
+++ 8.Functions.py
@@ -0,0 +1,9 @@
+#FUNCTIONS : Piece of codes to modulate the whole program and avoid repeatition of similar line of code.
+
+#To define a function we use the : 'def' keyword:
+def helloFunction():
+    pass #To leave a fucntion empty we need to write 'pass'. Else it will give error.
+
+print(helloFunction) #Gives the address , where the fucntion is created.
+print(helloFunction()) #Call the function , Output : 'None' , as fucntion does not return anything.
+print()
